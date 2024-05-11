4d2a3e763837b603ac6a2aec9a00ee288ea25059
wathika-eng <wathika02@gmail.com>
2024-05-11 23:53:17 +0300

    crud

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React from 'react';
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({ question, onDeleteClick, onAnswerChange }) {
+	const { id, prompt, answers = [], correctIndex } = question;
 
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
+	const options = answers.map((answer, index) => (
+		<option key={index} value={index}>
+			{answer}
+		</option>
+	));
 
-  return (
-    <li>
-      <h4>Question {id}</h4>
-      <h5>Prompt: {prompt}</h5>
-      <label>
-        Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
-      </label>
-      <button>Delete Question</button>
-    </li>
-  );
+	const handleDeleteClick = () => {
+		const del = window.confirm('Do you want to delete?');
+		if (del) {
+			onDeleteClick(id);
+		} else {
+			console.log('cancelled');
+		}
+	};
+
+	function handleAnswerChange(event) {
+		onAnswerChange(id, parseInt(event.target.value));
+	}
+
+	return (
+		<li>
+			<h4>Question {id}</h4>
+			<h5>Prompt: {prompt}</h5>
+			<label>
+				Correct Answer:
+				<select defaultValue={correctIndex} onChange={handleAnswerChange}>
+					{options}
+				</select>
+			</label>
+			<button onClick={handleDeleteClick}>Delete Question</button>
+		</li>
+	);
 }
 
 export default QuestionItem;
